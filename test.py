582f7afe75383244c31f0216dadc8bc377c0aa90
theli <theli.ua@gmail.com>
2012-08-26 21:57:20 +0300

    mhm

--- test.py
+++ test.py
@@ -7,19 +7,12 @@ dev = usb.core.find(idVendor=0x09da, idProduct=0x9090)
 if dev is None:
     raise ValueError('Device not found')
 
-for cfg in dev:
-    sys.stdout.write(str(cfg.bConfigurationValue) + '\n')
-    for intf in cfg:
-        sys.stdout.write('\t' + \
-                         str(intf.bInterfaceNumber) + \
-                         ',' + \
-                         str(intf.bAlternateSetting) + \
-                         '\n')
-        for ep in intf:
-            sys.stdout.write('\t\t' + \
-                             str(ep.bEndpointAddress) + \
-                             '\n')
-print hex(usb.util.CTRL_IN)
+ret = dev.ctrl_transfer(0x80, 0x06, 0xb504,0x1800, 1)
+ret = dev.ctrl_transfer(0x80, 0x06, 0xb500,0x9a00, 1)
+ret = dev.ctrl_transfer(0x80, 0x06, 0xb504,0xfe00, 1)
+#print [hex(x) for x in ret]
+ret = dev.ctrl_transfer(0x80, 0x06, 0xb501,0x1a00, 2)
+print [hex(x) for x in ret]
 ret = dev.ctrl_transfer(0x80, 0x06, 0xb600,0x0002, 8)
 print [hex(x) for x in ret]
 
